80877e3788efa9ce693a8ef82ae9f0482f5ed936
Anthony Tapia <ptonyptc@gmail.com>
2019-08-15 12:00:51 -0500

    methods initial and show-board done

--- main.rb
+++ main.rb
@@ -10,3 +10,33 @@ class Player
         @turn = turn
     end 
 end
+
+class Board
+    #main_board: Grid: 3rows, 3columns
+    attr_accessor :main_board
+
+    def initialize()
+        @main_board = [["1","2","3"],["4","5","6"],["7","8","9"]]
+    end  
+
+        #this function show the content of the grid
+    #for the beginning:
+    # | - | - | - |
+    # | 1 | 2 | 3 |
+    # | - | - | - |
+    # | 4 | 5 | 6 |
+    # | - | - | - |
+    # | 7 | 8 | 9 |
+    # | - | - | - |
+    def show_board()
+        puts ("| - | - | - |")
+        for i in 0..2 do
+            row = "|"
+            (3).times do |j|
+                row += " " + @main_board[i][j].to_s + " |"
+            end
+            puts row
+            puts "| - | - | - |"
+        end
+    end
+end
